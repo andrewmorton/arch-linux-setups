6f0de7ff5a4e0867c714f8510e571c2cd1dc5a62
Andrew Morton <ajmortonit@gmail.com>
2019-10-23 10:10:01 -0400

    Wed 23 Oct 2019 10:10:01 AM EDT backup of gentoo

--- gentoo-backups/tas-work/home/tas/.config/i3/home-xrandr-setup.sh
+++ gentoo-backups/tas-work/home/tas/.config/i3/home-xrandr-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xrandr --output HDMI-1 --off
-xrandr --output DP-1-3 --off
-xrandr --output DP-1-2 --off
+xrandr --output HDMI-1 --auto
+xrandr --output DP-1-3 --auto
+xrandr --output DP-1-2 --auto
 xrandr --output eDP-1 --primary --auto
