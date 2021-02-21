d4d85ad62a1fba978cef9fd40b31d16da53ced65
Andrew Morton <ajmortonit@gmail.com>
2021-02-21 17:18:35 -0500

    added bg pics for now

--- dotfiles/install_config_files.sh
+++ dotfiles/install_config_files.sh
@@ -9,9 +9,13 @@ if ! [ -d $HOME/.config/bspwm ] || ! [ -d $HOME/.config/sxhkd ]
 then
 	mkdir -p $HOME/.config/bspwm
 	mkdir -p $HOME/.config/sxhkd
+	mkdir -p $HOME/Pictures
 fi
 
 # Install all dotfiles for system
 ln -sf $this_dir/bashrc $HOME/.bashrc
 ln -sf $this_dir/bspwmrc $HOME/.config/bspwm/bspwmrc
 ln -sf $this_dir/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+
+# Make sym link to git pictures folder
+ln -sf $this_dir/bg $HOME/Pictures/bg
