8a2f8519006bf8ac72dbf61934e01d498dbf5920
Andrew Morton <ajmortonit@gmail.com>
2021-02-27 16:13:35 -0500

    Add better paclist logic and finished bin_scripts

--- user_scripts/bin_scripts/install_bin_scripts.py
+++ user_scripts/bin_scripts/install_bin_scripts.py
@@ -27,8 +27,28 @@ def remove_filetypes(root_path, install_script_name):
 
 
 def symlink_file(path, link_path):
-    #os.symlink(path, link_path)
+    if os.path.isfile(link_path):
+        print(f"{link_path} found in dst")
+        print("Forcing creation")
+        os.remove(link_path)
+    os.symlink(path, link_path)
     print(f"linked {path} to {link_path}.")
+
+
+def create_dir_if_missing(target_dir):
+    """
+    Tests if a given directory exists and creates if missing
+    """
+
+    if os.path.isdir(target_dir):
+        print(f"{target_dir} is found on this machine.")
+        print("Skipping creation.")
+        return False
+    else:
+        print(f"{target_dir} not found on this machine.")
+        print(f"Creating {target_dir}")
+        os.mkdir(target_dir)
+        return True
 
 
 def install_bin_scripts(bin_scripts_dir, target=f"{os.environ['HOME']}/bin"):
@@ -38,12 +58,13 @@ def install_bin_scripts(bin_scripts_dir, target=f"{os.environ['HOME']}/bin"):
     Sets assuming Unix environment but updates in Windows
     """
     
-    if platform.system() == "Windows":
-        target=f"{os.environ['HOMEPATH']}/bin"
+    working_path_set = copy.deepcopy(
+            remove_filetypes(bin_scripts_dir, sys.argv[0]))
 
-    print(remove_filetypes(bin_scripts_dir, sys.argv[0]))
+    create_dir_if_missing(target)
 
-    
+    for path in working_path_set:
+        symlink_file(path[0], f"{target}/{path[1]}")
+
 
 install_bin_scripts(".")
-
