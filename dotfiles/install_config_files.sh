1c89f152c1f2b266a7408a6b0747ef51b9ce3a05
Andrew Morton <ajmortonit@gmail.com>
2021-05-29 12:32:25 -0400

    [Update] path for emacs.el symlink

--- dotfiles/install_config_files.sh
+++ dotfiles/install_config_files.sh
@@ -19,7 +19,7 @@ ln -sf $this_dir/bspwmrc $HOME/.config/bspwm/bspwmrc
 ln -sf $this_dir/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 ln -sf $this_dir/init.vim $HOME/.config/nvim/init.vim
 ln -sf $this_dir/wired.ron $HOME/.config/wired/wired.ron
-ln -sf $this_dir/emacs $HOME/.emacs
+ln -sf $this_dir/emacs/emacs.el $HOME/.emacs.el
 ln -sf $this_dir/xbindkeysrc $HOME/.xbindkeysrc
 
 # Make sym link to git pictures folder
